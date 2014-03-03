26bad18cc77a1ce94dea27b716dce1c8b23e984a
ctrl-alt-delete <ctrl-alt-delete@live.ru>
2014-03-03 17:23:26 -0500

    author strings

--- tests.py
+++ tests.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: AxiaCore S.A.S. http://axiacore.com
+#
+# Based on ndef.parser, by Matthew Crumley (js-expression-eval)
+# https://github.com/silentmatt/js-expression-eval
+#
+# Ported to Python and modified by Vera Mazhuga (ctrl-alt-delete@live.com, http://vero4ka.info/)
+#
+# You are free to use and modify this code in anyway you find useful. Please leave this comment in the code
+# to acknowledge its original source. If you feel like it, I enjoy hearing about projects that use my code,
+# but don't feel like you have to let me know or ask permission.
+
 import unittest
 
 from parser import Parser
