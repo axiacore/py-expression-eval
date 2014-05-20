9425cca93476064bd3181dc743b7f096d8ef23a9
ctrl-alt-delete <ctrl-alt-delete@live.ru>
2014-05-20 08:01:44 -0500

    make py expression eval a python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python_expression_eval',
+    version='0.1',
+    description='Python Mathematical Expression Evaluator',
+    url='https://github.com/AxiaCore/py-expression-eval/',
+    author='Vera Mazhuga',
+    author_email='ctrl-alt-delete@live.ru',
+    license='MIT',
+    packages=['python_expression_eval'],
+    zip_safe=False,
+)
