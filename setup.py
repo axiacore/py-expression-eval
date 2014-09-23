5bd7cf0a30dc946cbe507fcdca7466e1c694a467
ctrl-alt-delete <ctrl-alt-delete@live.ru>
2014-09-23 14:40:16 -0500

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py_expression_eval',
-    version='0.1',
+    version='0.2',
     description='Python Mathematical Expression Evaluator',
     url='https://github.com/AxiaCore/py-expression-eval/',
     author='vero4ka',
