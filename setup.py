f8e72e2280104831a67267340c393711b5711918
Vera Mazhuga <vero4ka.ru@gmail.com>
2018-09-20 17:23:43 -0500

    Bumped version for 0.3.5 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py_expression_eval',
-    version='0.3.4',
+    version='0.3.5',
     description='Python Mathematical Expression Evaluator',
     url='https://github.com/AxiaCore/py-expression-eval/',
     author='vero4ka',
