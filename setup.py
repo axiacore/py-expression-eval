e7cfbedb3cdb1c428ae3dfbc967fe43deffa5e64
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-08-26 17:39:14 -0500

    Bumped version for 0.3.14 release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='py_expression_eval',
-    version='0.3.13',
+    version='0.3.14',
     description='Python Mathematical Expression Evaluator',
     long_description=long_description,
     long_description_content_type="text/markdown",
